7c70d287baaf321bb53aa9bce66ee803aa358882
Jacob Fiola <Jacob.Fiola@coxautoinc.com>
2020-10-22 09:16:46 -0600

    added stuff

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM ubuntu:16.04
 
-MAINTANER Jacob Fiola "jmfiola21@gmail.com"
-
 RUN apt-get update -y && \
     apt-get install -y python-pip python-dev
 
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-From flask import Flask
+from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
-    return 'Hey, we have Flask in a Docker container!'
+def hello_world():
+    return jsonify('Hey, we have Flask in a Docker container!')
 
-
-if __name == '__main__':
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
